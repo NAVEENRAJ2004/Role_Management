866082c8e34ec5ffc3ce26ce540fb896ca5b5a6a
NAVEENRAJ2004 <naveenraj10203040@gmail.com>
2025-03-28 20:05:18 +0530

    Revert "Update server.js"
    This reverts commit 2ca8d0aad400ea22d1b5a718d5d9d09c5bec4686.

--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 const allowedOrigins = [
-  'http://localhost:4200',
-  'https://mploychek-assignment.vercel.app'
+  'http://localhost:4200', // Local development
+  'https://mploychek-assignment-dz20bmv4p-naveenraj2004s-projects.vercel.app/' // Deployed frontend
 ];
 
 app.use(cors({
@@ -34,7 +34,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
