ff5051a6f90455d5b0523667d5fc2c15571a7234
NAVEENRAJ2004 <naveenraj10203040@gmail.com>
2025-03-28 20:17:50 +0530

    Revert "design changes"
    This reverts commit 1752ef3a670da010ab2a042cfed8287588423449.

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// Routes 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', usersRoutes);
 
